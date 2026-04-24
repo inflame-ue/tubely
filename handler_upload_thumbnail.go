098de54b17f7107fe721a82b6b729235c3276e8e
inflame-ue <inflamokil@gmail.com>
2026-04-24 12:29:16 +0300

    fix: the header was retuning mulitpart/form-data cause the file header was ignored

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"mime"
 	"net/http"
 	"os"
@@ -35,16 +36,6 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	mediaType, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Faield to parse the media type from content type", err)
-		return
-	}
-	if mediaType != "image/jpeg" && mediaType != "image/png" {
-		respondWithError(w, http.StatusBadRequest, "Invalid Content-Type header", err)
-		return
-	}
-
 	const maxMemory = 10 << 20
 	err = r.ParseMultipartForm(maxMemory)
 	if err != nil {
@@ -62,12 +53,23 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	file, _, err := r.FormFile("thumbnail")
+	file, header, err := r.FormFile("thumbnail")
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
 		return
 	}
 	defer file.Close()
+
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to parse the media type from content type", err)
+		return
+	}
+	log.Print(mediaType)
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Invalid Content-Type header", err)
+		return
+	}
 
 	fileExtension := strings.Split(mediaType, "/")[1]
 	fileName := fmt.Sprintf("%v.%v", videoIDString, fileExtension)
@@ -77,12 +79,13 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		respondWithError(w, http.StatusInternalServerError, "Failed to create the file", err)
 		return
 	}
+	defer writeFile.Close()
+
 	_, err = io.Copy(writeFile, file)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Failed to copy the file", err)
 		return
 	}
-	defer writeFile.Close()
 
 	thumbnailURL := fmt.Sprintf("http://localhost:%v/assets/%v", cfg.port, fileName)
 	video.ThumbnailURL = &thumbnailURL
